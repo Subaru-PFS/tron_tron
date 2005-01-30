0d422f5c5faa323033895865230e6deee149e6cd
cloomis <cloomis@astro.princeton.edu>
2005-01-30 16:59:14 +0000

    Added ping client Started a conversion module Added an external IDL subframer Split out TCC gcam command logic Added a maskfile manager.
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/trunk@65 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

---
+++ Nubs/ping.py
@@ -0,0 +1,29 @@
+import os.path
+
+import g
+import Hub
+import hub
+
+name = 'ping'
+
+def start(poller):
+
+    stop()
+
+    d = Hub.ASCIIReplyDecoder(debug=0)
+    e = Hub.ASCIICmdEncoder(debug=0, sendCommander=True)
+    nub = Hub.ShellNub(poller, ['/usr/bin/env',
+                                'PATH=/usr/local/bin:/usr/bin',
+                                'PYTHONPATH=%s/Client:%s' % (g.home, g.home),
+                                'clients/%s/%s.py' % (name, name)],
+                       name=name, encoder=e, decoder=d,
+                       #logDir=os.path.join(g.logDir, name),
+                       debug=0)
+    hub.addActor(nub)
+    
+def stop():
+    n = hub.findActor(name)
+    if n:
+        hub.dropActor(n)
+        del n
+
