ff432a49dbb92589092bbeb9720af656866ece6e
cloomis <cloomis@astro.princeton.edu>
2005-04-27 01:46:27 +0000

    Extended the meaning of the Actor.needsAuth field to allow specifying an actor for authentication purposes. Have disExpose, etc. require 'dis' permissions, etc. Have disExpose, etc. log their commands and responses.
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/trunk@91 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

--- Nubs/nicfps.py
+++ Nubs/nicfps.py
@@ -10,17 +10,16 @@ def start(poller):
     stop()
 
     initCmds = ('status',)
-
     safeCmds = r'status\s*$'
 
-    d = Hub.ASCIIReplyDecoder(EOL='\n', debug=5)
-    e = Hub.ASCIICmdEncoder(EOL='\n', debug=5)
+    d = Hub.ASCIIReplyDecoder(EOL='\n', debug=1)
+    e = Hub.ASCIICmdEncoder(EOL='\n', debug=1)
     nicfps = Hub.SocketActorNub(poller, 'nicfps', 9878,
                                 name=name, encoder=e, decoder=d,
                                 initCmds=initCmds, safeCmds=safeCmds,
                                 needsAuth=True,
                                 logDir=os.path.join(g.logDir, name),
-                                debug=5)
+                                debug=1)
     hub.addActor(nicfps)
     
 def stop():
