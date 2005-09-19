49604ba539e2547c1f10a0a100b32a7871ca5aca
cloomis <cloomis@astro.princeton.edu>
2005-09-19 16:24:08 +0000

    add nfocus (nicfps focus) stub routines.
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/trunk@120 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

---
+++ clients/guiders/nfocus.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+""" The "nfocus" command, which lets you invoke the PyGuide routines for arbitrary NICFPS images.
+
+"""
+
+import os
+import sys
+
+import client
+import CPL
+import Guider
+import GuideFrame
+
+sys.stderr.write("done imports\n")
+
+class nfocus(Guider.Guider):
+    def __init__(self, **argv):
+        sys.stderr.write("in nfocus.__init__\n")
+
+        cameraShim = None
+        Guider.Guider.__init__(self, cameraShim, 'nfocus', **argv)
+        
+    def _setDefaults(self):
+        Guider.Guider._setDefaults(self)
+        
+# Start it all up.
+#
+def main(name, eHandler=None, debug=0, test=False):
+    camActor = nfocus(tccGuider=False, debug=debug)
+    camActor.start()
+
+    client.run(name=name, cmdQueue=camActor.queue, background=False, debug=debug, cmdTesting=test)
+    CPL.log('nfocus.main', 'DONE')
+
+if __name__ == "__main__":
+    main('nfocus', debug=6)
