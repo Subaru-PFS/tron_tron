34e8212a4bb5073d918cfaee4523f1307363dbfa
cloomis <cloomis@astro.princeton.edu>
2006-08-09 16:03:37 +0000

    Manually add apollo before it adds itself.
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/trunk@235 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

--- runhub.py
+++ runhub.py
@@ -33,6 +33,7 @@ startAllConnections(['client',
 
 # Manually add ZA01 as an always-active commander of apollo.
 g.perms.addPrograms(['ZA01'])
+g.perms.addActors(['apollo'])
 g.perms.addActorsToProgram('ZA01', ['apollo'])
 
 hub.run()
