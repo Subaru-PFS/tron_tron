44cbdf149b8f062ee8cfd6fc221d76a857ae26f4
cloomis <cloomis@6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed>
2008-04-15 23:37:00 +0000

    Created config directory to safely keep guider configuration.
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/trunk@358 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

---
+++ clients/guiders/config/dcam.py
@@ -0,0 +1,48 @@
+# Where the camera files are saved.
+imageDir = 'dcam'
+imageRoot = '/export/images/'
+imageHost = 'tycho.apo.nmsu.edu'
+maskFile = '/export/images/keep/masks/dis/dcam.fits'
+biasFile = '/export/images/keep/masks/dis/dcam-bias.fits'
+
+requiredInst = 'DIS'
+requiredPort = 'NA2'
+guiderType = 'inst'
+imCtrName = 'IImCtr'
+imScaleName = 'IImScale'
+
+# Various attributes of the DIS slitviewer camera
+ccdSize = [512,512]
+binning = [1, 1]
+
+bias = 4400.0
+readNoise = 10.0 # unknown
+ccdGain = 4.5
+saturation = 60000
+
+# And of the guider
+
+# The PyGuide dataCut: sigma above background to consider a star.
+thresh = 2.5
+cradius = 35.0
+radMult = 1.2
+retry = 8
+restart = 'stop'
+
+vetoWithFindstars = False
+vetoLimit = 3.0
+doFlatfield = True
+doMaskfield = True
+doAutoDark = False
+
+minOffset = 0.1                         # Smallest offset we try to make, in arcseconds.
+
+# fitErrorScale uses the error estimate to scale the generated offsets.
+# The array contains pairs of numbers: an error threshhold, and a scale.
+# If the error falls with a given threshold, the corresponding factor is
+# applied to the offset. If the error exceeds the largest threshold, the offset
+# is zeroed.
+fitErrorScale = [(0.3, 0.8),
+                 (0.7, 0.5),
+                 (5.0, 0.3)]
+
---
+++ clients/guiders/config/tcam.py
@@ -0,0 +1,49 @@
+# Where the camera files are saved.
+imageDir = 'tcam'
+imageRoot = '/export/images/'
+imageHost = 'hub35m.apo.nmsu.edu'
+maskFile = '/export/images/keep/masks/tcam/tcam.fits'
+#biasFile = '/export/images/keep/masks/tcam/tcam-bias.fits'
+biasFile = None
+
+requiredInst = 'TSPEC'
+requiredPort = 'NA2'
+guiderType = 'inst'
+imCtrName = 'IImCtr'
+imScaleName = 'IImScale'
+
+# Various attributes of the TCAM slitviewer camera
+ccdSize = [1024,1024]
+binning = [2, 2]
+
+bias = 0.0
+readNoise = 10.0 # unknown
+ccdGain = 4.5
+saturation = 60000
+doFlatfield = True
+doMaskfield = True
+doAutoDark = False
+
+# And of the guider
+
+# The PyGuide dataCut: sigma above background to consider a star.
+thresh = 2.5
+cradius = 35.0
+radMult = 1.2
+retry = 8
+restart = 'stop'
+
+vetoWithFindstars = False
+vetoLimit = 3.0
+
+minOffset = 0.1                         # Smallest offset we try to make, in arcseconds.
+
+# fitErrorScale uses the error estimate to scale the generated offsets.
+# The array contains pairs of numbers: an error threshhold, and a scale.
+# If the error falls with a given threshold, the corresponding factor is
+# applied to the offset. If the error exceeds the largest threshold, the offset
+# is zeroed.
+fitErrorScale = [(0.3, 0.8),
+                 (0.7, 0.5),
+                 (5.0, 0.3)]
+
