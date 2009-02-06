1d02cfb4f964b8edb841221739a77c40eed42c69
cloomis <cloomis@astro.princeton.edu>
2009-02-06 18:29:38 +0000

    . use expDone exposureState for sequencing . pass gain= readrate= extsync= options through to the agile ICC expose command. . request dummy FITS cards from the ICC . use InstFITS.finishInto() to modify the dummy cards directly.
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/trunk@390 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

--- clients/xexpose/Exposure/ExpSequence.py
+++ clients/xexpose/Exposure/ExpSequence.py
@@ -2,7 +2,7 @@ import CPL
 import CPL.Exceptions.Error as Error
 
 import Actor
-import agileagExposure
+import agileExposure
 import disExposure
 import echelleExposure
 import grimExposure
@@ -26,7 +26,7 @@ class ExpSequence(Actor.Acting):
         self.expType = expType
         self.path = path
         self.cnt = cnt
-        self.cntLeft = cnt
+        self.cntDone = 0
         self.argv = argv
         self.state = "running"
 
@@ -57,10 +57,13 @@ class ExpSequence(Actor.Acting):
         # Possibly lie about how we are progressing
         #
         if self.startNum != None:
-            cnt = (self.cnt - self.cntLeft) + (self.startNum - 1)
+            cnt = self.cntDone+1 + (self.startNum - 1)
         else:
-            cnt = self.cnt - self.cntLeft
+            cnt = self.cntDone+1
 
+        if cnt > self.totNum:
+            cnt = self.totNum
+            
         state = self.state
         if state == 'done' and self.totNum != cnt:
             state = 'subsequence done'
@@ -108,33 +111,44 @@ class ExpSequence(Actor.Acting):
         """ Called when one of our exposures is finished.
         """
         
-        # If we actually generated image files, let them know.
-        if self.exposure and self.exposure.state not in ('idle', 'aborted'):
-            CPL.log("seq.finishExposure", "state=%s" % (self.exposure.state))
+        if self.exposure: expState = self.exposure.state
+        else: expState = None
+        #self.cmd.warn('debug=" _finishExposure cnt=%s cntDone=%s state=%s expstate=%s"' % (self.cnt, self.cntDone, self.state, expState))
 
-            # self.exposure.finishUp()
+        # If we actually generated image files, let them know.
+        if self.exposure and self.exposure.state not in ('idle', 'aborted', 'done'):
+            CPL.log("seq.finishExposure", "state=%s" % (self.exposure.state))
             self.reportExposure()
         # If we have reached the end of the sequence, close ourselves out.
-        if self.cntLeft <= 0 or self.state in ('stopped', 'aborted', 'done'):
+        if self.cntDone > self.cnt or self.state in ('stopped', 'aborted', 'done'):
             if self.state not in ('stopped', 'aborted', 'done'):
                 self.state = 'done'
             #CPL.log("seq.exposureFailed", "cnt left %s, state %s" % (str(self.cntLeft),self.state))
+            self.cmd.warn('debug=" _finishExposure shutting down cnt=%s cntDone=%s"' % (self.cnt, self.cntDone))
             self.returnKeys()
+            self.exposure = None
             self.actor.seqFinished(self)
             return
 
+        self.cntDone += 1
+        
         # Do not let pausing paper over 
         if self.state == 'paused':
             if self.exposure and self.exposure.state in ('done', 'aborted', 'failed'):
                 self.exposure = None
             return
- 
-        self.exposure = None
 
-    def _startExposure(self, firstExposure=False):
+        if not self.instDoesSequence:
+            self.exposure = None
+
+    def _startExposure(self):
         # Try-except -- CPL        
 
-        if firstExposure or not self.instDoesSequence:
+        if self.exposure: expState = self.exposure.state
+        else: expState = None
+        #self.cmd.warn('debug=" _startExposure cnt=%s cntDone=%s state=%s expstate=%s"' % (self.cnt, self.cntDone, self.state, expState))
+
+        if not self.exposure:
             try:
                 exec('instrumentExposure = %sExposure.%sExposure' % (self.inst, self.inst))
                 self.exposure = instrumentExposure(self.actor, self,
@@ -142,23 +156,27 @@ class ExpSequence(Actor.Acting):
             except Exception, e:
                 self.actor.seqFailed(self, "exposeTxt=%s" % (CPL.qstr(e)))
                 return
-        
-        self.cntLeft -= 1
+
         self.returnKeys()
         self.returnNewFilesKey()
-        self.exposure.run()
+
+        if not self.instDoesSequence or self.cntDone == 0:
+            self.exposure.run()
     
     def startSequence(self):
-        self._startExposure(firstExposure=True)
+        self._startExposure()
     
-    def nextInSequence(self):
+    def nextInSequence(self, done=False):
         """ Start the next exposure in the sequence.
 
         This is called after an exposure has finished.
         """
 
+        if done:
+            self.state = 'done'
+            
         self._finishExposure()
-        if self.cntLeft > 0 and self.state not in ('paused', 'stopped', 'aborted', 'done'):
+        if self.state not in ('paused', 'stopped', 'aborted', 'done'):
             self._startExposure()
         
     def exposureFailed(self, reason=""):
@@ -175,7 +193,7 @@ class ExpSequence(Actor.Acting):
             #CPL.log("seq.exposureFailed", "2")
             self.returnKeys()
             self.actor.seqFailed(self, reason)
-        
+
     def stop(self, cmd, **argv):
         self.state = "stopped"
         if self.exposure:
