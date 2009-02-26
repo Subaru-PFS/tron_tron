af847ef0f9ea111457fc33530e13c3e2adb12a01
cloomis <cloomis@astro.princeton.edu>
2009-02-26 19:32:06 +0000

    start at 3.5m -> 2.5m conversion
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/tags/SDSS3-pull@395 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

--- Hub/Reply/Decoders/__init__.py
+++ Hub/Reply/Decoders/__init__.py
@@ -1,5 +1,5 @@
 from ReplyDecoder import *
 from PyReplyDecoder import *
 from RawReplyDecoder import *
-from BinaryReplyDecoder import *
+#from BinaryReplyDecoder import *
 from ASCIIReplyDecoder import *
---
+++ Nubs/tcc25m.py
@@ -0,0 +1,48 @@
+import os.path
+
+import g
+import Hub
+import hub
+import IO
+
+name = 'tcc'
+
+def start(poller):
+
+    stop()
+
+    initCmds = ('show version',
+                'show users',
+                'show time',
+                'show status',
+                'show inst/full',
+                'show object/full',
+                'show axisconfig',
+                'show focus',
+                'axis status',
+                'mir status')
+
+    safeCmds = r"(^show )|(status$)"
+    
+    d = Hub.ASCIIReplyDecoder(EOL='\r',
+                              stripChars='\n',
+                              debug=1)
+    e = Hub.ASCIICmdEncoder(EOL='\r', debug=1)
+    tcc = Hub.TCCShellNub(poller, ['/usr/bin/ssh', '-1',
+                                   '-e', 'none', '-a', '-x',
+                                   '-i', '/home/cloomis/.ssh/tron', 
+                                   '-T', 'tccuser@tcc25m'],
+                          initCmds=initCmds, safeCmds=safeCmds,
+                          needsAuth=True,
+                          name=name, encoder=e, decoder=d,
+                          logDir=os.path.join(g.logDir, name),
+                          debug=1)
+
+    hub.addActor(tcc)
+    
+def stop():
+    n = hub.findActor(name)
+    if n:
+        hub.dropActor(n)
+        del n
+
