a84ff910bb2a391010f3f9907881657a8189d628
cloomis <cloomis@astro.princeton.edu>
2009-02-26 19:44:55 +0000

    2.5m cleanup
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/tags/SDSS3-pull@398 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

--- clients/guiders/config/gcamera.py
+++
@@ -1,8 +0,0 @@
-# Various attributes of the Alta camera
-cameraHostname = 'na2alta.apo.nmsu.edu'
-ccdSize = [1024,1024]
-bias = 1800.0                           # Very Suspect. See ecam.py
-readNoise = 21.3
-ccdGain = 1.6
-
-path = "/export/images/gcam"
