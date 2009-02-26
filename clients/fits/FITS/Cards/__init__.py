f6ff6eff268dfa2a8bbd947311be67791cc81792
cloomis <cloomis@6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed>
2009-02-26 19:41:07 +0000

    2.5m cleanup
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/tags/SDSS3-pull@396 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

--- clients/fits/FITS/Cards/__init__.py
+++
@@ -1,9 +0,0 @@
-from Card import Card
-
-from IntCard import IntCard
-from LogicalCard import LogicalCard
-from RealCard import RealCard
-from StringCard import StringCard
-from ValuelessCard import ValuelessCard
-
-__all__ = ["Card", "StringCard", "IntCard", "LogicalCard", "RealCard", "ValuelessCard"]
