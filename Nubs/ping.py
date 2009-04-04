3f3a84633e0e0d47c1508b3e557a06d156742477
cloomis <cloomis@6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed>
2009-04-04 01:52:48 +0000

    import path changes
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/branches/sdss3@418 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

--- Nubs/ping.py
+++ Nubs/ping.py
@@ -1,8 +1,11 @@
 import os.path
 
 import g
-import Hub
 import hub
+
+from Hub.Command.Encoders.ASCIICmdEncoder import ASCIICmdEncoder
+from Hub.Reply.Decoders.ASCIIReplyDecoder import ASCIIReplyDecoder
+from Hub.Nub.ShellNub import ShellNub
 
 name = 'ping'
 
@@ -10,15 +13,14 @@ def start(poller):
 
     stop()
 
-    d = Hub.ASCIIReplyDecoder(debug=0)
-    e = Hub.ASCIICmdEncoder(debug=0, sendCommander=True)
-    nub = Hub.ShellNub(poller, ['/usr/bin/env',
-                                'PATH=/usr/local/bin:/usr/bin',
-                                'PYTHONPATH=%s/Client:%s' % (g.home, g.home),
-                                'clients/%s/%s.py' % (name, name)],
-                       name=name, encoder=e, decoder=d,
-                       #logDir=os.path.join(g.logDir, name),
-                       debug=0)
+    d = ASCIIReplyDecoder(debug=0)
+    e = ASCIICmdEncoder(debug=0, sendCommander=True)
+    nub = ShellNub(poller, ['/usr/bin/env',
+#                            'PATH=/usr/local/bin:/usr/bin',
+                            'clients/%s/%s.py' % (name, name)],
+                   name=name, encoder=e, decoder=d,
+                   #logDir=os.path.join(g.logDir, name),
+                   debug=0)
     hub.addActor(nub)
     
 def stop():
