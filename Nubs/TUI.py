6adedd0ab4ac0f58425238a445e498fdf37df362
cloomis <cloomis@6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed>
2009-04-04 02:13:43 +0000

    adjust import paths
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/branches/sdss3@425 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

--- Nubs/TUI.py
+++ Nubs/TUI.py
@@ -1,7 +1,11 @@
 import socket
 import time
 
-import Hub
+from Hub.Command.Decoders.ASCIICmdDecoder import ASCIICmdDecoder
+from Hub.Reply.Encoders.ASCIIReplyEncoder import ASCIIReplyEncoder
+from Hub.Nub.Commanders import AuthStdinNub
+from Hub.Nub.Listeners import SocketListener
+
 import g
 import hub
 import os
@@ -15,23 +19,11 @@ def acceptTUI(in_f, out_f, addr=None):
     # Fetch a unique ID
     #
     nubID = g.nubIDs.gimme()
-
-    # This is gross, gross, gross. Basically, I want to stop the apollo
-    # traffic until it is requested. So I need to add a taster filtering call which
-    # specifies that, instead of enumerating all the acceptable ones. Ugh.
-    all = ('tcc','mcp',
-           'hub','msg')
-    all = ('*',)
+    fullname = '%s_%d' % (name, nubID)
     
-    allXX = ('dcam', 'ecam', 'gcam', 'tcam',
-             'dcamera', 'ecamera', 'gcamera', 'tcamera',
-             'disExpose', 'echelleExpose', 'nicfpsExpose',
-             'spicamExpose', 'tspecExpose',
-             'nicfps', 'dis', 'echelle','spicam', 'tspec',
-             'tcc', 'tlamps', 'hub', 'msg',
-             'perms', 'auth', 'fits',
-             'cm', 'nfocus', 'telmech', 'gmech',
-             'agile','agileExpose')
+    #all = ('tcc','mcp',
+    #       'hub','msg')
+    all = ('*',)
     
     otherIP, otherPort = in_f.getpeername()
     try:
@@ -41,23 +33,21 @@ def acceptTUI(in_f, out_f, addr=None):
 
     # os.system("/usr/bin/sudo /usr/local/bin/www-access add %s" % (otherIP))
         
-    d = Hub.ASCIICmdDecoder(needCID=False, EOL='\r\n', debug=1)
-    e = Hub.ASCIIReplyEncoder(EOL='\r', simple=True, debug=1, CIDfirst=True)
-    c = Hub.AuthStdinNub(g.poller, in_f, out_f,
-                         name='%s-%d' % (name, nubID), 
-                         encoder=e, decoder=d, debug=1,
-                         type='TUI', needsAuth=True,
-                         isUser=True,
-                         otherIP=otherIP, otherFQDN=otherFQDN)
+    d = ASCIICmdDecoder(needCID=False, EOL='\r\n', debug=1)
+    e = ASCIIReplyEncoder(EOL='\r', simple=True, debug=1, CIDfirst=True)
+    c = AuthStdinNub(g.poller, in_f, out_f,
+                     name=fullname,
+                     encoder=e, decoder=d, debug=1,
+                     type='TUI', needsAuth=True,
+                     isUser=True,
+                     otherIP=otherIP, otherFQDN=otherFQDN)
     c.taster.addToFilter(all, (), all)
     hub.addCommander(c)
     
 def start(poller):
-    reload(Hub)
-
     stop()
-
-    lt = Hub.SocketListener(poller, listenPort, name, acceptTUI)
+    
+    lt = SocketListener(poller, listenPort, name, acceptTUI)
     hub.addAcceptor(lt)
 
 def stop():
