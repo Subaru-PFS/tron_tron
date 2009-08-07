d9b1ae328f9f931704508202a9dbfb5886da65dc
cloomis <cloomis@astro.princeton.edu>
2009-08-07 16:33:32 +0000

    alerts actor
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/branches/sdss3@451 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

---
+++ Nubs/alerts.py
@@ -0,0 +1,35 @@
+import os.path
+
+from Hub.Command.Encoders.ASCIICmdEncoder import ASCIICmdEncoder
+from Hub.Reply.Decoders.ASCIIReplyDecoder import ASCIIReplyDecoder
+from Hub.Nub.SocketActorNub import SocketActorNub
+from Hub.Nub.Listeners import SocketListener
+import hub
+import g
+
+name = 'alerts'
+
+def start(poller):
+    stop()
+
+    initCmds = ('ping',
+                'status')
+
+    # safeCmds = r'^\s*info\s*$'
+
+    d = ASCIIReplyDecoder(debug=1)
+    e = ASCIICmdEncoder(sendCommander=True, useCID=False, debug=1)
+    nub = SocketActorNub(poller, 'hub25m.apo.nmsu.edu', 9995,
+                         name=name, encoder=e, decoder=d,
+                         grabCID=True, # the actor spontaneously generates a line we can eat.
+                         initCmds=initCmds, # safeCmds=safeCmds,
+                         needsAuth=False,
+                         logDir=os.path.join(g.logDir, name),
+                         debug=1)
+    hub.addActor(nub)
+    
+def stop():
+    n = hub.findActor(name)
+    if n:
+        hub.dropActor(n)
+        del n
