0ddd554d920a4419e8f514be946e1594a3b721f3
cloomis <cloomis@6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed>
2010-02-03 03:50:57 +0000

    add weather nub
    git-svn-id: svn://svn.apo.nmsu.edu/tron/tron/branches/sdss3@491 6cb9dee2-cd90-4b54-8ea6-2d2f74f8b5ed

---
+++ Nubs/weather.py
@@ -0,0 +1,36 @@
+import os.path
+
+from Hub.Command.Encoders.ASCIICmdEncoder import ASCIICmdEncoder
+from Hub.Reply.Decoders.ASCIIReplyDecoder import ASCIIReplyDecoder
+from Hub.Nub.SocketActorNub import SocketActorNub
+from Hub.Nub.Listeners import SocketListener
+import hub
+import g
+
+name = 'weather'
+
+def start(poller):
+    stop()
+
+    initCmds = ('ping',
+                'version',
+                'status')
+
+    # safeCmds = r'^\s*info\s*$'
+
+    d = ASCIIReplyDecoder(debug=3)
+    e = ASCIICmdEncoder(sendCommander=True, useCID=False, debug=3)
+    nub = SocketActorNub(poller, 'hub25m.apo.nmsu.edu', 9990,
+                         name=name, encoder=e, decoder=d,
+                         grabCID=True, # the actor spontaneously generates a line we can eat.
+                         initCmds=initCmds, # safeCmds=safeCmds,
+                         needsAuth=False,
+                         logDir=os.path.join(g.logDir, name),
+                         debug=3)
+    hub.addActor(nub)
+    
+def stop():
+    n = hub.findActor(name)
+    if n:
+        hub.dropActor(n)
+        del n
