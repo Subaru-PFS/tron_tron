3f11554fea5ba73290bc3d34d5a80c9aec34f92d
Craig Loomis <cloomis@astro.princeton.edu>
2013-04-23 10:26:52 -0400

    replace sample actor code with MCS snapshot

---
+++ Docs/sphinx/mcs_keys.py
@@ -0,0 +1,12 @@
+KeysDictionary('mcs', (1, 3),
+               Key("text", String(help="text for humans")),
+               Key("version", String(help="EUPS/git version")),
+               Key("cameraName", String()),
+               Key("readNoise", Float(help="nominal readnoise for camera", units="e-")),
+               Key("ccdTemp", Float(help="measured temperature at CCD mount", units="degC")),
+               Key("centroidsChunk", String(help="base64 encoded centroids")),
+               Key('exposureState',
+                   Enum('IDLE','FLUSHING','INTEGRATING','READING','ABORTED','DONE' name="state"),
+                   help='The current state of the exposure'),
+               Key('filename', String(help='name of just finished exposure')),
+)
